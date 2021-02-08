4f06be18b991cf61e8ec97a12a4f6a8065149240
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-07 20:18:00 -0800

    Add passing test1 for substitue().

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
 // Business Logic
 
-// _Describe: increment()_
-// _Test-1: "It parses input to a number "_
-// _Expect(increment("4")).toEqual(4)_
-
-// _Test-2: "It increments a numbersArray of numbers from zero to input"_
-// _Expect(increment("4")).toEqual([0, 1, 2, 3, 4])_
-
 // let numbersArray = [];
 function increment(input) {
   inputNumber = parseInt(input);
@@ -18,7 +11,16 @@ function increment(input) {
 // numbersArray;
 // returns: (5) [0, 1, 2, 3, 4]
 
+// _Describe: substitute()_
+// _Test-1: "It iterates through numbersArray and stringifies the numbers"_
+// _Expect(substitute(4)).toEqual(["0", "1", "2", "3", "4"])_
 
+// let numbersArray = [0, 1, 2, 3, 4]
+const stringifiedArray = numbersArray.map(function(number) {
+  return number.toString();
+});
+// stringifiedArray;
+// returns: (5) ["0", "1", "2", "3", "4"]
 
 
 // User Interface Logic  
