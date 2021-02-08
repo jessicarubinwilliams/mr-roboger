e3210e98b2ba59c353582ccc17bcd02eac1fa1cb
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-07 20:56:57 -0800

    Add passing test2 for substitute().

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
 // Business Logic
 
-// let numbersArray = [];
+let numbersArray = [];
 function increment(input) {
   inputNumber = parseInt(input);
   for (let index = 0; index <= inputNumber; index++) {
     numbersArray.push(index)
   }  
 };
-// increment("4");
-// numbersArray;
-// returns: (5) [0, 1, 2, 3, 4]
+increment("13");
 
-// _Describe: substitute()_
-// _Test-1: "It iterates through numbersArray and stringifies the numbers"_
-// _Expect(substitute(4)).toEqual(["0", "1", "2", "3", "4"])_
-
-// let numbersArray = [0, 1, 2, 3, 4]
 const stringifiedArray = numbersArray.map(function(number) {
   return number.toString();
 });
-// stringifiedArray;
-// returns: (5) ["0", "1", "2", "3", "4"]
 
+const roboArray = stringifiedArray.map (function(string) {
+  if  (string.includes("3") === true) {
+    return string = "Won't you be my neighbor?";
+  } if (string.includes("2") === true) {
+    return string = "Boop!";
+  } if (string.includes("1") === true) {
+    return string = "Beep!";
+  } else {
+    return string;
+  }
+});
+roboArray;
 
 // User Interface Logic  
 $(document).ready(function() {
